c843f5e0daa182149ec0801fcc3cd3e9df0b2ed5
Shaun <me@technicallyshaun.com>
2026-01-24 08:54:00 +0000

    Scaffold basic dashboard and entry point

---
+++ internal/tui/dashboard.go
@@ -0,0 +1,36 @@
+package tui
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+}
+
+func NewModel() Model {
+	return Model{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	return fmt.Sprintf(
+		"Welcome to crAIzy!\n\n%s\n",
+		"(Press q to quit)",
+	)
+}
