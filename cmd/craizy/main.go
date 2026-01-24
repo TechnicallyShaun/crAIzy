8b06fb273c33705e664c531a14b475e7d9a03519
Shaun <me@technicallyshaun.com>
2026-01-24 09:10:17 +0000

    Add help flag

--- cmd/craizy/main.go
+++ cmd/craizy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,6 +10,17 @@ import (
 )
 
 func main() {
+	help := flag.Bool("help", false, "Show help message")
+	flag.BoolVar(help, "h", false, "Show help message")
+	flag.Parse()
+
+	if *help {
+		fmt.Println("Usage: craizy [flags]")
+		fmt.Println("\nFlags:")
+		flag.PrintDefaults()
+		return
+	}
+
 	p := tea.NewProgram(tui.NewModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
